7e2d48cfbe61df18f0da4a8f09e0a07be2d0e056
Roberto Alvarez <robertoatc02@gmail.com>
2014-02-09 22:48:21 -0600

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,10 @@
 # Author Roberto Alvarez (robertoatc02@gmail.com)
 # Default is World!
 
-name = ARGV.first || "World"
+#name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
 
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
